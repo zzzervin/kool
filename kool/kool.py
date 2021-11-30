3adb9eb5aedd2e02669301d4a4e2de220e216152
Ervin_Liiva <opilane@HP-CZC6297B8P>
2021-11-30 15:24:18 +0200

    Почтовый индекс

--- kool/kool.py
+++ kool/kool.py
@@ -1,19 +1,43 @@
-Kmd=[]#tühi järjend
+#Kmd=[]#tühi järjend
 
-Km=10# 
-print("1.paeval pikkus oli",Km)
-Kmd.append(Km)
-print(Kmd)
-for p in range(2,8):
-    Km*=1.1
-    print(round(Km,2))
-    Kmd.append(round(Km,2))
-print(Kmd)
-print(Kmd[2])
-print(Kmd.index(16.11))
-Kmd.remove(10)#pop(0)
-Kmd.append(199)
+#Km=10# 
+#print("1.paeval pikkus oli",Km)
+#Kmd.append(Km)
+#print(Kmd)
+#for p in range(2,8):
+    #Km*=1.1
+    #print(round(Km,2))
+    #Kmd.append(round(Km,2))
+#print(Kmd)
+#print(Kmd[2])
+#print(Kmd.index(16.11))
+#Kmd.remove(10)#pop(0)
+#Kmd.append(199)
+#print(Kmd)
+#print(Kmd.count(16.11))
+#print(len(Kmd))
 
-print(Kmd)
-print(Kmd.count(16.11))
-print(len(Kmd))
+#Maakonnad=["Tallin","Narva","Narva-jõusu","Kahla-Jarve","Ida-Viruma, Lääne-Virumaa, Jõgevamaa","Lääneituma","Jõgevamaa","Tartu linn","Tartumaa, Põlvamaa, Võrumaa, Valgamaa","Viljandimaa, Järvamaa, Harjumaa, Raplamaa","Pärnumaa","Läänemaa, Hiiumaa, Saaremaa"]
+#while 1:
+#    try:
+#        Index=int(input())
+#        if len(str(Index))==5:
+#            break
+#    except ValueError:
+#        print("ei")
+#i_list=list(str(Index))
+#s1=int(i_list[0])#1->0,2->1
+#print(str(Maakonnad[s1-1]))
+#if s1 in[1,2,3]:
+#    print("Jatke kodus!")
+#else:
+#     print("Kanna maski")
+
+
+
+
+
+
+
+
+
